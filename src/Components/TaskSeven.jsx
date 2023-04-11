9f108fb19755a043055c9b0e1253b2e6e7b4c130
Nagarjun Enumudi Venkatesha <nagarjunev.11@gmail.com>
2023-04-11 16:20:03 -0500

    implemented task7

--- src/Components/TaskSeven.jsx
+++ src/Components/TaskSeven.jsx
@@ -4,6 +4,82 @@ import React, { useEffect, useState, useRef } from 'react';
 
 function TaskSeven() {
   // YOUR CODE HERE
+
+  const [inputList, setInputList] = useState([]);
+  const [userInput, setUserInput] = useState('');
+
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalText, setModalText] = useState('');
+
+  const modalRef = useRef();
+
+  const addUserInput = () => {
+    setInputList([...inputList, userInput]);
+    setUserInput('');
+  };
+
+  const openModal = (str) => {
+    setModalOpen(true);
+    setModalText(str);
+  };
+
+  useEffect(() => {
+    const clickOutside = (e) => {
+      if (modalOpen && !modalRef?.current.contains(e.target))
+        setModalOpen(false);
+    };
+    window.addEventListener('mousedown', clickOutside);
+
+    return () => {
+      window.removeEventListener('mousedown', clickOutside);
+    };
+  }, [modalOpen]);
+
+  return (
+    <>
+      <div style={{ textAlign: 'center' }}>
+        <div>
+          <input
+            value={userInput}
+            type="text"
+            onChange={(e) => setUserInput(e.target.value)}
+          />
+          <div style={{ marginTop: 10 }}>
+            <button disabled={!userInput} onClick={addUserInput}>
+              Add names
+            </button>
+          </div>
+        </div>
+        <div>
+          {inputList &&
+            inputList.map((str, i) => (
+              <p key={i} onClick={() => openModal(str)}>
+                {String(str).length > 5
+                  ? `${String(str).substring(0, 5)}...`
+                  : str}
+              </p>
+            ))}
+        </div>
+        <div>
+          {modalOpen && (
+            <div
+              ref={modalRef}
+              style={{
+                position: 'absolute',
+                inset: 0,
+                margin: 'auto',
+                height: 700,
+                width: 500,
+                background: 'black',
+              }}
+            >
+              <p> Full text : {modalText}</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default TaskSeven;
